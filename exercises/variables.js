67ea97f99ef4403cfbc8957fc1608af56836635b
rimager <rima.gerhard@gmail.com>
2016-01-22 16:45:38 -0500

    Update variables.js

--- exercises/variables.js
+++ exercises/variables.js
@@ -1,5 +1,5 @@
 /*
-The Fortune Teller
+0. The Fortune Teller
 Why pay a fortune teller when you can just program your fortune yourself?
 - Store the following into variables:
   number of children, partner's name, geographic location, job title.
@@ -7,4 +7,41 @@ Why pay a fortune teller when you can just program your fortune yourself?
   "You will be a X in Y, and married to Z with N kids."
 */
 
-// write your solution here...
+/*
+1. The Age Calculator
+Forgot how old someone is? Calculate it!
+- Store the current year in a variable.
+- Store their birth year in a variable.
+- Calculate their 2 possible ages based on the stored values.
+- Output them to the screen like so: "They are either NN or NN", substituting the values.
+*/
+
+/*
+2. The Lifetime Supply Calculator
+Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+- Store your current age into a variable.
+- Store a maximum age into a variable.
+- Store an estimated amount per day (as a number).
+- Calculate how many you would eat total for the rest of your life.
+- Output the result to the screen like so:
+"You will need NN to last you until the ripe old age of X".
+*/
+
+/*
+3. The Geometrizer
+Calculate properties of a circle, using the definitions here:
+http://math2.org/math/geometry/circles.htm
+- Store a radius into a variable.
+- Calculate the circumference based on the radius, and output "The circumference is NN".
+- Calculate the area based on the radius, and output "The area is NN".
+*/
+
+/*
+4. The Temperature Converter
+It's hot out! Let's make a converter based on the steps here:
+http://www.mathsisfun.com/temperature-conversion.html
+- Store a celsius temperature into a variable.
+- Convert it to fahrenheit and output "NN°C is NN°F".
+- Now store a fahrenheit temperature into a variable.
+- Convert it to celsius and output "NN°F is NN°C."
+*/
