9123c5aaf77131b4202b8da9c5cb59b1d77602b6
rimager <rima.gerhard@gmail.com>
2016-01-22 20:25:48 -0500

    Create class2.js

---
+++ class-code/class2.js
@@ -0,0 +1,36 @@
+console.log("TGIF!!");
+
+var name;
+name = "Rima G.";
+
+var email = "rima.gerhard@gmail.com";
+
+var hometown = "Miami Beach";
+hometown = 1;
+
+var tgif = true;
+
+var restaurant = 'Rima\'s Place';
+var greeting = "\"Oh Hi\"";
+console.log(restaurant);
+console.log(greeting);
+
+var username = prompt("What is your name?");
+alert("Hello " + username + "!");
+
+//Alert a greeting to the user's name
+
+
+// var breaktime = "breakk!!!";
+// if ("break?"){
+// 	alert("It's breaktime");
+// }
+// else{
+// 	alert("Uh oh more coding");
+// }
+
+
+/* Problem 4*/
+var celsius = 39;
+var celsiusToFahrenheit = 5 * celsius;
+alert(celsius + " is " + celsiusToFahrenheit + "in F");
