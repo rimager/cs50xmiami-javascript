150d7a6b24b79170118711ca74657b6a9ba40df3
rimager <rima.gerhard@gmail.com>
2016-01-29 12:22:08 -0500

    Create controlflow.js

---
+++ exercises/controlflow.js
@@ -0,0 +1,42 @@
+/*
+What number's bigger?
+Write a function named greaterNum that:
+- takes 2 arguments, both numbers.
+- returns whichever number is the greater (higher) number.
+Call that function 2 times with different number pairs,
+and log the output to make sure it works (e.g. "The greater number of 5 and 10 is 10.").
+*/
+
+/*
+The World Translator
+Write a function named helloWorld that:
+- takes 1 argument, a language code (e.g. "es", "de", "en")
+- returns "Hello, World" for the given language, for at least 3 languages.
+  It should default to returning English.
+Call that function for each of the supported languages
+and log the result to make sure it works.
+*/
+
+/*
+The Grade Assigner
+Write a function named assignGrade that:
+- takes 1 argument, a number score.
+- returns a grade for the score, either "A", "B", "C", "D", or "F".
+Call that function for a few different scores and log the result to make sure it works.
+*/
+
+/*
+The Pluralizer
+Write a function named pluralize that:
+- takes 2 arguments, a noun and a number.
+- returns the number and pluralized form, like "5 cats" or "1 dog".
+Call that function for a few different scores and log the result to make sure it works.
+- Bonus: Make it handle a few collective nouns like "sheep" and "geese".
+*/
+
+/*
+The even/odd reporter
+- Write a for loop that will iterate from 0 to 20.
+- For each iteration, it will check if the current number is even or odd,
+  and report that to the screen (e.g. "2 is even").
+*/
