b11d9754756af2170328acf085077b02bf3b04e2
rimager <rima.gerhard@gmail.com>
2016-02-05 15:04:31 -0500

    Update bom.js

--- exercises/bom.js
+++ exercises/bom.js
@@ -1,1 +1,7 @@
-
+/*
+Create a popup window displaying your favorite website, initializing it at the top left of the user's screen.
+Size it 100px x 100px.
+Move it to the center of the current browser window.
+Resize the window to 500px x 500px.
+Give the popup window focus.
+*/
