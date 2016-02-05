b725e8f91e7eae8e05f8267e0537ce08fa64a172
rimager <rima.gerhard@gmail.com>
2016-02-05 16:28:14 -0500

    Update dom.js

--- exercises/dom.js
+++ exercises/dom.js
@@ -1,4 +1,9 @@
 /*
+Use Javascript to determine whether the user visited other sites previous to yours.
+If they did, and a Back button to your website.
+*/
+
+/*
 Implement a Javascript clock using Timeouts.
 It should display the current hh:mm:ss and update the second count every second.
 Embed the clock on an HTML website.
