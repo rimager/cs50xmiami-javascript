baf26b1d7c2381f8ba2916b4b3f9d17e046da3f4
rimager <rima.gerhard@gmail.com>
2016-02-05 17:28:11 -0500

    Create functions-solution.js

---
+++ exercises/functions-solution.js
@@ -0,0 +1,54 @@
+/*
+The Fortune Teller part 2
+Why pay a fortune teller when you can just program your fortune yourself?
+Write a function named tellFortune that:
+- takes 4 arguments: number of children, partner's name, geographic location, job title.
+- outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+- Call that function 3 times with 3 different values for the arguments.
+*/
+
+/*
+The Age Calculator part 2
+Forgot how old you are? Calculate it!
+Write a function named calculateAge that:
+- takes 2 arguments: birth year, current year.
+- calculates the 2 possible ages based on those years.
+- outputs the result to the screen like so: "You are either NN or NN"
+- Call the function three times with different sets of values.
+- Bonus: Figure out how to get the current year in JavaScript instead of passing it in.
+*/
+
+/*
+The Lifetime Supply Calculator part 2
+Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+Write a function named calculateSupply that:
+- takes 2 arguments: age, amount per day.
+- calculates the amount consumed for rest of the life (based on a constant max age).
+- outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
+- Call that function three times, passing in different values each time.
+- Bonus: Accept floating point values for amount per day, and round the result to a round number.
+*/
+
+/*
+The Geometrizer part 2
+Create 2 functions that calculate properties of a circle, using the definitions here:
+http://math2.org/math/geometry/circles.htm
+Create a function called calcCircumfrence:
+- Pass the radius to the function.
+- Calculate the circumference based on the radius, and output "The circumference is NN".
+Create a function called calcArea:
+- Pass the radius to the function.
+- Calculate the area based on the radius, and output "The area is NN".
+*/
+
+/*
+The Temperature Converter part 2
+It's hot out! Let's make a converter based on the steps here:
+http://www.mathsisfun.com/temperature-conversion.html
+Create a function called celsiusToFahrenheit:
+- Store a celsius temperature into a variable.
+- Convert it to fahrenheit and output "NN°C is NN°F".
+Create a function called fahrenheitToCelsius:
+- Now store a fahrenheit temperature into a variable.
+- Convert it to celsius and output "NN°F is NN°C."
+*/
