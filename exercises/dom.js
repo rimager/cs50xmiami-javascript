928078876c314b57b02278c35c84b06ceb76e545
rimager <rima.gerhard@gmail.com>
2016-02-05 17:21:42 -0500

    Update dom.js

--- exercises/dom.js
+++ exercises/dom.js
@@ -8,3 +8,8 @@ Implement a Javascript clock using Timeouts.
 It should display the current hh:mm:ss and update the second count every second.
 Embed the clock on an HTML website.
 */
+
+/*
+Create an HTML List (http://www.w3schools.com/tags/tag_ul.asp).
+Ask the user for their to do items. Each time an item is given, add it to the list.
+*/
