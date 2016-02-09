e98a5db21c9c1ab6e29c47216f756713538e3630
rimager <rima.gerhard@gmail.com>
2016-02-09 11:46:30 -0500

    Create arrays.js

---
+++ exercises/arrays.js
@@ -0,0 +1,5 @@
+/*
+Your top choices
+Create an array to hold your top choices (colors, presidents, whatever).
+For each choice, log to the screen a string like: "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
+*/
