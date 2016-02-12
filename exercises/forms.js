adaebaf003f5237b338fa8535fc14d1f33ee02c0
rimager <rima.gerhard@gmail.com>
2016-02-12 15:26:42 -0500

    Create forms.js

---
+++ exercises/forms.js
@@ -0,0 +1,22 @@
+/*
+Program a text field (an <input type="text"> tag) that letters cannot be typed into. Only numbers are allowed.
+*/
+
+
+/*
+Extend a text field so that when the user types, a list of suggested values is shown below the field. You have an array of possible values available and should show those that start with the text that was typed. When a suggestion is clicked, replace the text field’s current value with it.
+
+<input type="text" id="field">
+<div id="suggestions" style="cursor: pointer"></div>
+
+<script>
+  // Builds up an array with global variable names, like
+  // 'alert', 'document', and 'scrollTo'
+  var terms = [];
+  for (var name in window)
+    terms.push(name);
+
+  // Your code here.
+</script>
+*/
+
