e41770d47972cbd7381707e7507012b2a390dc4e
rimager <rima.gerhard@gmail.com>
2016-02-12 16:40:24 -0500

    Update forms.js

--- exercises/forms.js
+++ exercises/forms.js
@@ -1,4 +1,42 @@
 /*
+Validate all fields on the following form with Javascript.
+ <form id="contactForm" action="" >
+      <fieldset>
+        <legend>Contact Details</legend>
+        <p>
+          <label for="custname">Name</label> 
+          <input id="custname" name="custname" />
+          Required
+        </p>
+        <p>
+          <label for="company">Company</label> 
+          <input id="company" name="company" type="text" />
+          Optional
+        </p>
+        <p>
+          <label for="email">Email</label> 
+          <input id="email" name="email" type="text" />
+          Required
+        </p>
+        <p>
+          <label for="telephone">Telephone</label> 
+          <input id="telephone" name="telephone" type="text" />
+          Optional
+        </p>
+        <p>
+          <label for="website">Website</label> 
+          <input id="website" name="website" type="text" />
+          Required
+        </p>
+        <p>
+          <input class="submit" type="submit" name="submit" value="Submit Contact Details" />
+          <input type="reset" value="clear" />
+        </p>
+      </fieldset>
+    </form>
+*/
+
+/*
 Program a text field (an <input type="text"> tag) that letters cannot be typed into. Only numbers are allowed.
 */
 
