4a2dcbf01632935ef7545307bbc58ae71aa20d0c
rimager <rima.gerhard@gmail.com>
2016-02-19 18:01:11 -0500

    Create ajax.js

---
+++ exercises/ajax.js
@@ -0,0 +1,12 @@
+/*
+Implement the Yahoo Weather API
+https://developer.yahoo.com/weather/
+Make an Ajax call to the API and display the result on a website.
+*/
+
+
+/*
+Implement the Google Books API
+https://developers.google.com/books/
+Make an Ajax call to the API and display a Book Info.
+*/
