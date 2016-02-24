a6a32c643774d071759522e216e4de6b422a8af3
rimager <rima.gerhard@gmail.com>
2016-02-23 19:33:19 -0500

    Create security.js

---
+++ exercises/security.js
@@ -0,0 +1,9 @@
+/*
+Use Ajax and JSONP to display your Github data using http://githubbadge.appspot.com/
+Display your languages, last_project, last_project_url, repo and follower count.
+Sample URL: http://githubbadge.appspot.com/rimager
+*/
+
+/*
+XSS
+*/
