d229731c3767111ff4a528848ae0417dda0378ee
rimager <rima.gerhard@gmail.com>
2016-02-26 20:41:29 -0500

    Update jquery.js

--- exercises/jquery.js
+++ exercises/jquery.js
@@ -2,3 +2,8 @@
 Rewrite one of your projects using XMLHttpRequest to jQuery's ajax function.
 http://api.jquery.com/jquery.ajax/
 */
+
+/*
+Complete the basic slider presented in class.
+http://codepen.io/rimager/pen/MyWejO
+*/
